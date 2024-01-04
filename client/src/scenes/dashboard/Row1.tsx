15c3f591b066addf6f7fa9b4c072ef5b5322448f
varshigak <varshigak@gmail.com>
2024-01-03 23:41:13 -0500

    Add KPI routes

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -4,6 +4,8 @@ import { useGetKpisQuery } from '@/state/api'
 
 const Row1 = () => {
     const { data } = useGetKpisQuery();
+    console.log('data', data)
+    
   return (
     <>
         <DashboardBox gridArea="a"></DashboardBox>
