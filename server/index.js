79f6d29aef8f2d404e030da2f9c1963d50834ee0
varshigak <varshigak@gmail.com>
2024-01-07 04:06:34 -0500

    Create product and transaction schemas, routes, and models

--- server/index.js
+++ server/index.js
@@ -6,8 +6,10 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js"
+import productRoutes from "./routes/product.js";
+import Product from "./models/Product.js"
 import KPI from "./models/KPI.js";
-import { kpis } from "./data/data.js";
+import { kpis, products } from "./data/data.js";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -22,6 +24,7 @@ app.use(cors());
 
 /* ROUTES */
 app.use("/kpi", kpiRoutes);
+app.use("/product", productRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
@@ -36,5 +39,6 @@ mongoose
         /* ADD DATA ONE TIME ONLY OR AS NEEDED */
         // await mongoose.connection.db.dropDatabase();
         // KPI.insertMany(kpis);
+        // Product.insertMany(products);
     })  
     .catch((error) => console.log(`${error} did not connect`))
