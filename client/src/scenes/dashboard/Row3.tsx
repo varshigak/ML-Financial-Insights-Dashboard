cdd31d28af68044d8cf7abe0dc6883ec16c4db85
varshigak <varshigak@gmail.com>
2024-01-07 04:19:01 -0500

    Add product data retrieval query

--- client/src/scenes/dashboard/Row3.tsx
+++ client/src/scenes/dashboard/Row3.tsx
@@ -1,14 +1,14 @@
-import DashboardBox from '@/components/DashboardBox'
+import DashboardBox from "@/components/DashboardBox";
 
 const Row3 = () => {
   return (
     <>
-        <DashboardBox gridArea="g"></DashboardBox>
-        <DashboardBox gridArea="h"></DashboardBox>
-        <DashboardBox gridArea="i"></DashboardBox>
-        <DashboardBox gridArea="j"></DashboardBox>
+      <DashboardBox gridArea="g"></DashboardBox>
+      <DashboardBox gridArea="h"></DashboardBox>
+      <DashboardBox gridArea="i"></DashboardBox>
+      <DashboardBox gridArea="j"></DashboardBox>
     </>
-)
-}
+  );
+};
 
-export default Row3
+export default Row3;
--- client/src/state/types.ts
+++ client/src/state/types.ts
@@ -1,34 +1,46 @@
 export interface ExpensesByCategory {
-    salaries: number;
-    supplies: number;
-    services: number;
+  salaries: number;
+  supplies: number;
+  services: number;
 }
 
 export interface Month {
-    id: string;
-    month: string;
-    revenue: number;
-    expenses: number;
-    nonOperationalExpenses: number;
-    operationalExpenses: number;
+  id: string;
+  month: string;
+  revenue: number;
+  expenses: number;
+  nonOperationalExpenses: number;
+  operationalExpenses: number;
 }
 
 export interface Day {
-    id: string;
-    date: string;
-    revenue: number;
-    expenses: number;
+  id: string;
+  date: string;
+  revenue: number;
+  expenses: number;
 }
 
 export interface GetKpisResponse {
-    id: string;
-    _id: string;
-    __v: number;
-    totalProfit: number;
+  id: string;
+  _id: string;
+  __v: number;
+  totalProfit: number;
+  totalRevenue: number;
+  totalExpenses: number;
+  expensesByCategory: ExpensesByCategory;
+  monthlyData: Array<Month>;
+  dailyData: Array<Day>;
+  createdAt: string;
+  updatedAt: string;
+}
 
-    totalRevenue: number;
-    totalExpenses: number;
-    expensesByCategory: ExpensesByCategory;
-    monthlyData: Array<Month>;
-    dailyData: Array<Day>;
+export interface GetProductsResponse {
+  id: string;
+  _id: string;
+  __v: number;
+  price: number;
+  expense: number;
+  transactions: Array<string>;
+  createdAt: string;
+  updatedAt: string;
 }
