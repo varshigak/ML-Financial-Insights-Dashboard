d337952babb8b587324c7385179a0ee55be7893f
varshigak <varshigak@gmail.com>
2024-01-07 05:50:49 -0500

    Add API route to fetch transactions

--- server/index.js
+++ server/index.js
@@ -5,13 +5,15 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-import kpiRoutes from "./routes/kpi.js"
+import kpiRoutes from "./routes/kpi.js";
 import productRoutes from "./routes/product.js";
-import Product from "./models/Product.js"
+import transactionRoutes from "./routes/transaction.js";
+import Product from "./models/Product.js";
 import KPI from "./models/KPI.js";
-import { kpis, products } from "./data/data.js";
+import Transaction from "./models/Transaction.js";
+import { kpis, products, transactions } from "./data/data.js";
 
-/* CONFIGURATIONS */
+/ CONFIGURATIONS /;
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -22,23 +24,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-/* ROUTES */
+/ ROUTES /;
 app.use("/kpi", kpiRoutes);
 app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes);
 
-/* MONGOOSE SETUP */
+/ MONGOOSE SETUP /;
 const PORT = process.env.PORT || 9000;
-mongoose 
-    .connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(async () => {
-        app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-        /* ADD DATA ONE TIME ONLY OR AS NEEDED */
-        // await mongoose.connection.db.dropDatabase();
-        // KPI.insertMany(kpis);
-        // Product.insertMany(products);
-    })  
-    .catch((error) => console.log(`${error} did not connect`))
+    / ADD DATA ONE TIME ONLY OR AS NEEDED /;
+    // await mongoose.connection.db.dropDatabase();
+    // KPI.insertMany(kpis);
+    // Product.insertMany(products);
+    // Transaction.insertMany(transactions);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
